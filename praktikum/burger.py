414f55c8372efce641a99e8851136d6624a76e3d
Kseniya Minchenko <mahamaham28@yandex.ru>
2024-07-10 01:48:51 +0300

    commit1

--- praktikum/burger.py
+++ praktikum/burger.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from praktikum.bun import Bun
-from praktikum.ingredient import Ingredient
+from .ingredient import Ingredient
 
 
 class Burger:
@@ -9,7 +9,7 @@ class Burger:
     Модель бургера.
     Бургер состоит из булочек и ингредиентов (начинка или соус).
     Ингредиенты можно перемещать и удалять.
-    Можно распечать чек с информацией о бургере.
+    Можно распечатать чек с информацией о бургере.
     """
 
     def __init__(self):
